876281b35ac746d48a14827f3a766b185c55e7fb
llthompson <lisalacythompson+github@gmail.com>
2023-05-13 16:30:23 -0500

    build contact page

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
