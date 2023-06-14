a5c7304c317873b47d34a045f3582951895b5e71
llthompson <lisalacythompson+github@gmail.com>
2023-06-13 20:29:17 -0500

    nav responsive

--- nav.js
+++ nav.js
@@ -2,9 +2,9 @@ const menuToggle = document.getElementById('menuToggle');
 const menu = document.getElementById('menu');
 
 menuToggle.addEventListener('change', function () {
-    if (menu.style.display === 'block') {
+    if (menu.style.display === 'flex') {
         menu.style.display = 'none';
     } else {
-        menu.style.display = 'block';
+        menu.style.display = 'flex';
     }
 });
